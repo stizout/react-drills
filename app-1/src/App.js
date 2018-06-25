82b245c64e1cd1be401cc18f8b139d9d07326c44
Alex Stout <stizout@gmail.com>
2018-06-25 15:35:15 -0700

    Finsh app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: 'This is where my text will go',
+    }
+  }
+
+  updateText(input) {
+    this.setState({input})
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e) => this.updateText(e.target.value)} />
+        <p>{this.state.input}</p>
       </div>
     );
   }
