095fde9c678a92af392f9ee7b6f780eb0dc81bd3
Alex Stout <stizout@gmail.com>
2018-06-25 15:52:40 -0700

    complete assignment

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      arr: ["Spaghetti", "Ice Cream", 'Sushi', 'Bologna', 'Cheese']
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.arr.map((item) => <div>{item}</div>)}
       </div>
     );
   }
