1b72ecb344042498a1fa13af515bbe5b5f29e60a
Alex Stout <stizout@gmail.com>
2018-06-25 20:00:55 -0700

    complete assignment

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  updateUsername(username) {
+    this.setState({username})
+  }
+
+  updatePassword(password) {
+    this.setState({password})
+  }
+
+  alertStuff() {
+    alert("Username: " + this.state.username + " Password: " + this.state.password)
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +29,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input placeholder="Username" onChange={(e) => this.updateUsername(e.target.value)}/>
+        <input placeholder="Password" onChange={(e) => this.updatePassword(e.target.value)}/>
+        <button onClick={() => this.alertStuff()}>Login</button>
       </div>
     );
   }
