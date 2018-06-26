31aff317d90521ad64617f3a740ea2c45e5dd835
Alex Stout <stizout@gmail.com>
2018-06-25 17:19:21 -0700

    complete assignment

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      arr: ["Spaghetti", "Ice Cream", 'Sushi', 'Bologna', 'Cheese'],
+      input: '',
+    }
+  }
+
+  updateInput(input) {
+    this.setState({input})
+    console.log(this.state.input)
+  }
+
   render() {
+    let display = this.state.arr.filter((e) => {
+      return e.includes(this.state.input)
+    }).map((e) => {
+      return <p>{e}</p>
+    })
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          
+        <div>
+          <input onChange={(e) => this.updateInput(e.target.value)}/>
+          { display }
+        </div>
       </div>
     );
   }
