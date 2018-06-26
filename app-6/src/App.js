22f5f39ab4c1100d41851851281074294c09aa46
Alex Stout <stizout@gmail.com>
2018-06-25 20:25:58 -0700

    complete assignment

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.addNewTask = this.addNewTask.bind(this);
+    this.submitNewTask = this.submitNewTask.bind(this);
+    this.state = {
+      tasks: ["Trash", "Groceries", "Sleep"],
+      newTask: '',
+    }
+  }
+
+  addNewTask(newTask) {
+    this.setState({newTask})
+  }
+
+  submitNewTask() {
+    this.setState((prevState) => {
+      return {
+        tasks: prevState.tasks.concat(this.state.newTask),
+        newTask: ''
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +33,27 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Tasks
+          key={this.state.input}
+            tasks={this.state.tasks}
+            addNewTask={this.addNewTask}
+            submitNewTask={this.submitNewTask} 
+          />
       </div>
     );
   }
+}
+
+const Tasks = (props) => {
+  return (
+    <div>
+      <input onChange={(e) => props.addNewTask(e.target.value)} name="task"/>
+      <button onClick={() => props.submitNewTask()}>Submit</button>
+      {props.tasks.map((e) => {
+        return <div>{e}</div>
+      })}
+    </div>
+  )
 }
 
 export default App;
